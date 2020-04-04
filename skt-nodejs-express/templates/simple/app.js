ebffbe568afdae93572bf6195e7cbd743cbc96c2
user1 <user1@kebhana.com>
2020-04-04 20:21:11 +0900

    change nodejs-express

--- skt-nodejs-express/templates/simple/app.js
+++ skt-nodejs-express/templates/simple/app.js
@@ -1,7 +1,44 @@
-const app = require('express')()
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const serveStatic = require("serve-static");
 
-app.get('/', (req, res) => {
-  res.send("Hello from Appsody!");
+global.__BASEDIR = __dirname + '/';
+
+const app = express();
+
+app.use(
+  serveStatic(path.join(__dirname, "public"), {
+    maxAge: "1m"
+  })
+);
+app.use(express.static(path.join(__BASEDIR, '/public')));		//static resource 폴더 
+app.use(bodyParser.urlencoded({extended:false}));				//include request 객체 parser
+
+app.use(function(req, res, next) {
+  let pathname = req.url;
+  
+  if(pathname === "/") {
+    let html = "";
+    html = "<h1>Appsody nodejs-express Sample</h1>"+
+    "<ul>"+
+    "<li><a href=\"/\" target=\"_self\">Home</a></li>"+
+    "<li><a href=\"/appmetrics-dash\" target=\"_New\">Dashboard</a></li>"+
+    "<li><a href=\"/live\" target=\"_New\">Liveness</a></li>"+
+    "<li><a href=\"/ready\" target=\"_New\">Readiness</a></li>"+
+    "<li><a href=\"/health\" target=\"_New\">Health check</a></li>"+
+    "<li><a href=\"/metrics\" target=\"_New\">Metrics</a></li>"+
+    "</ul>";
+
+
+    res.writeHead(200, { 'Content-Type':'text/html; charset=utf-8' });
+    res.write(html);
+    res.end();
+  } else {
+    next();
+  }
+
 });
  
 module.exports.app = app;
+
